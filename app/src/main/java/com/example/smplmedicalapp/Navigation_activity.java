f3dd7209e4e4bd03e0c14f3fda5d961247ac1c47
Subhojitpaul559 <paulsubhojit016@gmail.com>
2020-12-07 10:12:48 +0530

    initial

--- app/src/main/java/com/example/smplmedicalapp/Navigation_activity.java
+++ app/src/main/java/com/example/smplmedicalapp/Navigation_activity.java
@@ -1,14 +1,23 @@
 package com.example.smplmedicalapp;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class Navigation_activity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_activity);
+
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().hide();
+
+            
+        }
     }
 }
