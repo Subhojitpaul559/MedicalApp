6742271e6c5634d1a682904d251e1d0e1dc8c425
DIYelectronicS99 <diyelectronics99@gmail.com>
2021-01-03 00:10:41 +0530

    Search update | add item size update v02

---
+++ app/src/main/java/com/example/smplmedicalapp/SearchAdapter.java
@@ -0,0 +1,153 @@
+package com.example.smplmedicalapp;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ExampleViewHolder> implements  Filterable{
+    private List<SearchModel> exampleList;
+    private List<SearchModel> exampleListFull;
+
+    @Override
+    public Filter getFilter() {
+        return examplefilter;
+    }
+
+    public class ExampleViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView imageView;
+        public TextView name, desc, price, qty, discount, size, org_price, discountPrice ;
+        public Button button;
+        public RelativeLayout relativeLayout;
+
+        public ExampleViewHolder(View itemView){
+            super(itemView);
+            this.imageView = itemView.findViewById(R.id.item_img);
+            this.name = itemView.findViewById(R.id.item_name);
+            this.desc = itemView.findViewById(R.id.item_desc);
+            this.org_price = itemView.findViewById(R.id.item_original_price);
+            this.discountPrice = itemView.findViewById(R.id.item_price);
+            this.discount = itemView.findViewById(R.id.item_discount);
+            this.qty = itemView.findViewById(R.id.item_qty);
+            this.size = itemView.findViewById(R.id.item_size);
+            this.button = itemView.findViewById(R.id.btn_remove);
+            button.setVisibility(View.INVISIBLE);
+            relativeLayout = itemView.findViewById(R.id.item_relativeLayout);
+        }
+
+    }
+    public SearchAdapter(List<SearchModel> exampleList) {
+        this.exampleList = exampleList;
+        exampleListFull = new ArrayList<>(exampleList);
+    }
+    @NonNull
+    @Override
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_item_layout, parent, false);
+        return new ExampleViewHolder(v);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder holder, int position) {
+        SearchModel currentItem = exampleList.get(position);
+        //holder.imageView.setImageResource(currentItem.getImageResource());
+        //holder.textView2.setText(currentItem.getText2());
+
+
+        holder.name.setText(currentItem.getName());
+        holder.desc.setText(currentItem.getDescription());
+        holder.org_price.setText("₹"+currentItem.getPrice());
+        holder.discountPrice.setText("₹"+currentItem.getDprice());
+        holder.qty.setText("Qty :"+ currentItem.getQty());
+        holder.discount.setText(currentItem.getDiscount()+"%off");
+        holder.size.setText("Size :"+ currentItem.getSize());
+
+//        holder.button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseDatabase.getInstance().getReference().child("items")
+//                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+//                        .child(getRef(position).getKey())
+//                        .removeValue();
+//                Toast.makeText( v.getContext(), "Deleted !", Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageReference = storage.getReferenceFromUrl("gs://smplmedicalapp-408ea.appspot.com/Images/").child(currentItem.getImage());
+        File file = null;
+        try {
+            file = File.createTempFile("image", "jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File finalFile = file;
+        storageReference.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Bitmap bitmap = BitmapFactory.decodeFile(finalFile.getAbsolutePath());
+                holder.imageView.setImageBitmap(bitmap);
+            }
+        });
+
+    }
+    @Override
+    public int getItemCount() {
+        return exampleList.size();
+    }
+
+    private Filter examplefilter= new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<SearchModel> filterlist=new ArrayList<>();
+            if(constraint==null|| constraint.length()==0){
+                filterlist.addAll(exampleListFull);
+            }
+            else{
+                String pattrn=constraint.toString().toLowerCase().trim();
+                for(SearchModel item :exampleListFull){
+                    if(item.getName().toLowerCase().contains(pattrn)){
+                        filterlist.add(item);
+                    }
+                }
+            }
+            FilterResults filterResults=new FilterResults();
+            filterResults.values=filterlist;
+            return filterResults;
+
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            exampleList.clear();
+            exampleList.addAll((List)results.values);
+            notifyDataSetChanged();
+        }
+    };
+}
---
+++ app/src/main/java/com/example/smplmedicalapp/SearchModel.java
@@ -0,0 +1,85 @@
+package com.example.smplmedicalapp;
+
+public class SearchModel {
+
+
+    private  String name, description, image, price, discount, qty, size, dprice ;
+
+
+    public SearchModel(String name, String description,  String image, String price, String dprice, String size, String qty, String discount) {
+
+        this.name = name;
+        this.image = image;
+        this.description = description;
+        this.price = price;
+        this.dprice = dprice;
+        this.discount = discount;
+        this.qty = qty;
+        this.size = size;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getDprice() {
+        return dprice;
+    }
+
+    public void setDprice(String dprice) {
+        this.dprice = dprice;
+    }
+}
